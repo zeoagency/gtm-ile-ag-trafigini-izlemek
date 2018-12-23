ac107cdea11cd481cb77ff903aa00f1facf19599
Zeo Agency <osman@zeo.org>
2018-12-23 23:48:51 +0300

    Update script.js

--- gsheets/script.js
+++ gsheets/script.js
@@ -15,13 +15,13 @@ function _getParams(response) {
   // Param list: name, company, email, phone, cpref, clid
   //----------------------------------------------//
   var params = response.parameter
-  , name = params["name"]
-  , company = params['company']
-  , email = params["email"]
-  , phone = params["phone"]
-  , contactPreference = params["cpref"]
-  , clid = params["clid"]
-  , date = Utilities.formatDate(new Date(), "GMT+3", "yyyy-MM-dd'T'HH:mm:ss'Z'");
+    , name = params["name"]
+    , company = params['company']
+    , email = params["email"]
+    , phone = params["phone"]
+    , contactPreference = params["cpref"]
+    , clid = params["clid"]
+    , date = Utilities.formatDate(new Date(), "GMT+3", "yyyy-MM-dd'T'HH:mm:ss'Z'");
   //----------------------------------------------//
   return [[date, name, company, email, phone, contactPreference, clid]];
 }
@@ -29,8 +29,8 @@ function _getParams(response) {
 //----------------------------------------------//
 function _insertNewRow(eventData) {
   var targetSheet = SpreadsheetApp.getActiveSheet()
-  , startRow = targetSheet.getLastRow() + 1
-  , fieldCount = eventData[0].length;
+    , startRow = targetSheet.getLastRow() + 1
+    , fieldCount = eventData[0].length;
   targetSheet.getRange(startRow, 1, 1, fieldCount).setValues(eventData);
   //----------------------------------------------//
   return;
