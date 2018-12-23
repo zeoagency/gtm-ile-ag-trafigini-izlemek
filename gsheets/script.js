b36f5e46323e9912c8b830010e7a23df51ed9c92
Zeo Agency <osman@zeo.org>
2018-12-23 23:47:52 +0300

    Create script.js

---
+++ gsheets/script.js
@@ -0,0 +1,37 @@
+//------------------- Örnek Apps Script makro.
+function doGet(e) {
+	try	{
+		var eventData = _getParams(e);
+		_insertNewRow(eventData);
+		return HtmlService.createHtmlOutput(eventData.join(' '));
+	} catch (e) {
+		return HtmlService.createHtmlOutput('Error occured:', e.toString());
+	}
+}
+
+//----------------------------------------------//
+function _getParams(response) {
+	//----------------------------------------------//
+	// Param list: name, company, email, phone, cpref, clid
+	//----------------------------------------------//
+	var params = response.parameter
+		, name = params["name"]
+        , company = params['company']
+		, email = params["email"]
+		, phone = params["phone"]
+		, contactPreference = params["cpref"]
+		, clid = params["clid"]
+		, date = Utilities.formatDate(new Date(), "GMT+3", "yyyy-MM-dd'T'HH:mm:ss'Z'");
+	//----------------------------------------------//
+	return [[date, name, company, email, phone, contactPreference, clid]];
+}
+
+//----------------------------------------------//
+function _insertNewRow(eventData) {
+	var targetSheet = SpreadsheetApp.getActiveSheet()
+		, startRow = targetSheet.getLastRow() + 1
+		, fieldCount = eventData[0].length;
+	targetSheet.getRange(startRow, 1, 1, fieldCount).setValues(eventData);
+	//----------------------------------------------//
+	return;
+}
