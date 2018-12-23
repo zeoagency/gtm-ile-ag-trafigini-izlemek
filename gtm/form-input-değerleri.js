1d455bc9ceb4e45c5af6b0a4b108e2b64f3afd30
Zeo Agency <osman@zeo.org>
2018-12-23 23:44:49 +0300

    Update form-input-değerleri.js

--- gtm/form-input-değerleri.js
+++ gtm/form-input-değerleri.js
@@ -1,4 +1,5 @@
-//------------- Örnek script.
+//------------- Örnek Custom javascript
+
 function () {
 	try {
 		var name = document.querySelector('input[name="your-name"]').value;
@@ -8,20 +9,20 @@ function () {
 		var contactButtons = document.querySelectorAll('input[name="iletisim-tercihi"]');
 		var contactPreference = 'Phone';
 		[].forEach.call(contactButtons, function(button) {
-			if (button.getAttribute('checked') === 'checked') {
-				contactPreference = button.getAttribute('value');
-			}
+		if (button.getAttribute('checked') === 'checked') {
+			contactPreference = button.getAttribute('value');
+		}
 		})
 		return [
-				['name=', name],
-				['company=', company],
-				['email=', email],
-				['phone=', phone],
-				['cpref=', contactPreference]
-			]
-			.map(function(param) {return param.join('')})
-			.join('&');
+			['name=', name],
+			['company=', company],
+			['email=', email],
+			['phone=', phone],
+			['cpref=', contactPreference]
+		]
+		.map(function(param) {return param.join('')})
+		.join('&');
 	} catch (e) {
-		return false;
+	return false;
 	}
 }
