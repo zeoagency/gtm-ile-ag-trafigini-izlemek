36d13b18aff507340cd89597df14fcc317561882
Zeo Agency <osman@zeo.org>
2018-12-27 00:20:26 +0300

    Update decodeuri.js

--- gtm/decodeuri.js
+++ gtm/decodeuri.js
@@ -1,4 +1,4 @@
-//----------- Örnek Custom Javascript.
+//----------- Örnek Custom JavaScript değişkeni.
 function() {
   var imageURL = 'https://script.google.com/macros/s/AKfycbzZOWVITxWHjnjcTl3jX-JL_-DwnMA1oHmbidvxaxaxasAS/exec?'
     + {{Form Bilgileri}}
